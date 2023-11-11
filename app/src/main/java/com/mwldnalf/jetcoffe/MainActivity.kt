7f5e30fbc8ceb90bb8ff1958fe247a1bc896dea3
wilelilac <wildfahreza@gmail.com>
2023-11-12 06:05:05 +0700

    Banner and Category

--- app/src/main/java/com/mwldnalf/jetcoffe/MainActivity.kt
+++ app/src/main/java/com/mwldnalf/jetcoffe/MainActivity.kt
@@ -3,13 +3,29 @@ package com.mwldnalf.jetcoffe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mwldnalf.jetcoffe.components.CategoryItem
+import com.mwldnalf.jetcoffe.components.Search
+import com.mwldnalf.jetcoffe.components.SectionText
+import com.mwldnalf.jetcoffe.model.dummyCategory
 import com.mwldnalf.jetcoffe.ui.theme.JetCoffeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    JetCoffeeApp("Android")
+                    JetCoffeeApp()
                 }
             }
         }
@@ -30,17 +46,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun JetCoffeeApp(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        Search()
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier.padding(16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryRowPreview() {
+    JetCoffeTheme {
+        CategoryRow()
+    }
+}
+
+@Composable
+fun JetCoffeeApp(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun JetCoffeeAppPreview() {
     JetCoffeTheme {
-        JetCoffeeApp("Android")
+        JetCoffeeApp()
     }
 }
