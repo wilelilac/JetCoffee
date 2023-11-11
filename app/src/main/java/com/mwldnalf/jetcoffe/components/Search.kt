1fa6387c471319c85fb6fc08ec10ca7596156e77
wilelilac <wildfahreza@gmail.com>
2023-11-12 06:22:34 +0700

    Banner and Category

--- app/src/main/java/com/mwldnalf/jetcoffe/components/Search.kt
+++ app/src/main/java/com/mwldnalf/jetcoffe/components/Search.kt
@@ -11,45 +11,45 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.mwldnalf.jetcoffe.ui.theme.JetCoffeTheme
+import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
 import com.google.android.material.search.SearchBar
 import com.mwldnalf.jetcoffe.R
-import com.mwldnalf.jetcoffe.ui.theme.JetCoffeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Search(modifier: Modifier = Modifier) {
-//    SearchBar(
-//        query = "",
-//        onQueryChange = {},
-//        onSearch = {},
-//        active = false,
-//        onActiveChange = {},
-//        leadingIcon = {
-//            Icon(
-//                imageVector = Icons.Default.Search,
-//                contentDescription = null,
-//                tint = MaterialTheme.colorScheme.onSurfaceVariant
-//            )
-//        },
-//        placeholder = {
-//            Text(stringResource(R.string.placeholder_search))
-//        },
-//        shape = MaterialTheme.shapes.large,
-//        colors = SearchBarDefaults.colors(
-//            containerColor = MaterialTheme.colorScheme.background
-//        ),
-//        modifier = modifier
-//            .padding(16.dp)
-//            .fillMaxWidth()
-//            .heightIn(min = 48.dp)
-//    ) {
-//    }
+    SearchBar(
+        query = remember { mutableStateOf("") },
+        onQueryChange = {},
+        onSearch = {},
+        active = false,
+        onActiveChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+        },
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        shape = MaterialTheme.shapes.large,
+        colors = SearchBarDefaults.colors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+    )
 }
+
 
 @Preview(showBackground = true)
 @Composable
