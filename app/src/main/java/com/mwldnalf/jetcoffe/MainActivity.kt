7bb11cc219cd1b0a6e40c9dd4d5734754026c7ad
wilelilac <wildfahreza@gmail.com>
2023-11-13 10:04:43 +0700

    Banner and Category

--- app/src/main/java/com/mwldnalf/jetcoffe/MainActivity.kt
+++ app/src/main/java/com/mwldnalf/jetcoffe/MainActivity.kt
@@ -23,9 +23,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mwldnalf.jetcoffe.components.CategoryItem
+import com.mwldnalf.jetcoffe.components.MenuItem
 import com.mwldnalf.jetcoffe.components.Search
 import com.mwldnalf.jetcoffe.components.SectionText
+import com.mwldnalf.jetcoffe.model.Menu
+import com.mwldnalf.jetcoffe.model.dummyBestSellerMenu
 import com.mwldnalf.jetcoffe.model.dummyCategory
+import com.mwldnalf.jetcoffe.model.dummyMenu
 import com.mwldnalf.jetcoffe.ui.theme.JetCoffeTheme
 
 class MainActivity : ComponentActivity() {
@@ -76,6 +80,22 @@ fun CategoryRow(
 }
 
 @Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Composable
 @Preview(showBackground = true)
 fun CategoryRowPreview() {
     JetCoffeTheme {
@@ -89,6 +109,10 @@ fun JetCoffeeApp(modifier: Modifier = Modifier) {
         Banner()
         SectionText(stringResource(R.string.section_category))
         CategoryRow()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
     }
 }
 
